4f7b3d3746000a1a84ddb035945480df9a9262be
Elvan Hidayat <elvanhd@gmail.com>
2023-03-19 17:48:12 +0700

    create a custom fetch hooks

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,31 +1,22 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
 import './TripList.css';
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState('http://localhost:3000/trips');
-
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url);
-    const trips = await response.json();
-    setTrips(trips);
-  }, [url]);
-
-  useEffect(() => {
-    console.log('useEffect function ran');
-    fetchTrips();
-  }, [fetchTrips]);
+  const { data: trips } = useFetch(url);
 
   return (
     <div className='trip-list'>
       <h2>Trip List</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className='filters'>
         <button
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setData(data);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data };
+};
