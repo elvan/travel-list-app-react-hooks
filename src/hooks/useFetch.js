dde9f94d1a1feca7ea7aac505c15565f13cb0efe
Elvan Hidayat <elvanhd@gmail.com>
2023-03-19 17:56:53 +0700

    add a loading state

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,17 +2,21 @@ import { useEffect, useState } from 'react';
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPending(true);
+
       const res = await fetch(url);
       const data = await res.json();
 
+      setIsPending(false);
       setData(data);
     };
 
     fetchData();
   }, [url]);
 
-  return { data };
+  return { data, isPending };
 };
